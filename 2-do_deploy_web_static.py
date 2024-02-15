3a483e13ec01d327db3e5c9b7d62d6b692bbe85a
m-mwangi <127857750+m-mwangi@users.noreply.github.com>
2024-02-15 22:22:35 +0000

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,57 +1,49 @@
 #!/usr/bin/python3
-"""
-    Fabric script that distributes archive to web servers
-"""
-from fabric.api import env, put, run, local
-from os.path import exists, isdir
-import os.path
-import re
+"""Fab script that distributes an archives"""
 
 
-env.user = 'ubuntu'
+from datetime import datetime
+from fabric.api import *
+import os
+
 env.hosts = ['52.87.185.174', '34.228.162.170']
-env.key_filename = '~/.ssh/id_rsa'
+env.user = "ubuntu"
+
+
+def do_pack():
+    """
+        that distributes an archive to web servers
+    """
+
+    local("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    archived_f_path = "versions/web_static_{}.tgz".format(date)
+    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
+
+    if t_gzip_archive.succeeded:
+        return archived_f_path
+    else:
+        return None
 
 
 def do_deploy(archive_path):
-    """
-        Distributes archive to web servers
-    """
-    if not exists(archive_path):
-        return False
+    """Archives to web-servers"""
+    if os.path.exists(archive_path):
+        archived_file = archive_path[9:]
+        newest_version = "/data/web_static/releases/" + archived_file[:-4]
+        archived_file = "/tmp/" + archived_file
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(newest_version))
+        run("sudo tar -xzf {} -C {}/".format(archived_file,
+                                             newest_version))
+        run("sudo rm {}".format(archived_file))
+        run("sudo mv {}/web_static/* {}".format(newest_version,
+                                                newest_version))
+        run("sudo rm -rf {}/web_static".format(newest_version))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(newest_version))
 
-    put(archive_path, "/tmp/")
-    filename = re.search(r'[^/]+$', archive_path).group(0)
-    folder = "/data/web_static/releases/{}".format(
-        os.path.splitext(filename)[0])
+        print("New version deployed!")
+        return True
 
-    if not exists(folder):
-        run("mkdir -p {}".format(folder))
-
-    # Extract files from archive
-    run("tar -xzf /tmp/{} -C {}".format(filename, folder))
-
-    # Remove archive from web server
-    run("rm /tmp/{}".format(filename))
-
-    # Move all files from web_static to the new folder
-    run("mv {}/web_static/* {}".format(folder, folder))
-
-    # Remove the web_static folder
-    run("rm -rf {}/web_static".format(folder))
-
-    # Delete the symbolic link
-    run("rm -rf /data/web_static/current")
-
-    # Create new symbolic link
-    run("ln -s {} /data/web_static/current".format(folder))
-
-    # Create 'hbnb_static' directory if it doesn't exist
-    if not isdir("/var/www/html/hbnb_static"):
-        run("sudo mkdir -p /var/www/html/hbnb_static")
-
-    # Sync 'hbnb_static' with 'current'
-    run("sudo cp -r /data/web_static/current/* /var/www/html/hbnb_static/")
-
-    print("New version deployed!")
-    return True
+    return False
